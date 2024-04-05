719a34a3ef3a4919b35342f3a2309f3e1982185e
Zakariya'u Dauda <84882261+zayd137@users.noreply.github.com>
2024-04-05 21:04:37 +0100

    Add files via upload

---
+++ Stage-two/Zayd/setup.sh
@@ -0,0 +1,48 @@
+#The fastqc used for quality control. To install:
+
+conda install -c bioconda fastqc
+
+#or
+
+sudo apt-get install fastqc
+
+#The multiqc: efficient for multiple dataser. To install:
+
+conda install -c bioconda multiqc
+
+#or
+
+sudo apt-get install multiqc
+
+#The fastp tool  used for trimming. To install:
+
+conda install -c bioconda fastp
+
+#or
+
+sudo apt-get install fastp
+
+#The  bwa tool used for genome mapping. To install:
+
+conda install -c bioconda bwa
+
+#or
+
+sudo apt-get install bwa
+
+#The  samtools. To install:
+
+conda install -c bioconda samtools
+
+#or
+
+sudo apt-get install samtools
+
+#The freebayes tool: To install
+
+conda install -c bioconda freebayes
+
+or
+
+sudo apt-get install freebayes
+
