1f34e88af8674baaad17dd70135161f744380a97
Bruno Chikere <162906840+BrunoChi01@users.noreply.github.com>
2024-04-05 18:15:08 +0100

    Add files via upload

---
+++ Stage-two/Bruno/script.sh
@@ -0,0 +1,47 @@
+mkdir stage2
+cd stage2
+wget https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz
+wget https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
+mkdir ref
+cd ref
+wget https://zenodo.org/records/10886725/files/Reference.fasta
+cd stage2
+cd ..
+cd ref
+sudo apt-get install fastqc
+cd ..
+ls *.gz
+fastqc *.fastq.gz
+pwd
+sudo apt-get install fastp
+pwd
+rm *trimmed
+fastp -i *_1.fastq.gz -I *_2.fastq.gz -o trimmed_R1.fastq -O trimmed_R2.fastq --qualified_quality_phred 20 --html report.html --json report.json
+pwd
+fastqc trimmed_*.fastq
+pwd
+cd ref
+sudo apt-get install bwa
+bwa index -a bwtsw Reference.fasta
+cd ..
+bwa mem ref/Reference.fasta trimmed_R1.fastq trimmed_R2.fastq > ERR8774458.sam
+cd ref
+sudo apt-get install samtools
+cd ..
+samtools view -hbo ERR8774458.bam ERR8774458.sam
+samtools sort ERR8774458.bam -o ERR8774458.sorted.bam
+samtools index ERR8774458.sorted.bam
+sudo apt-get install bcftools
+bcftools mpileup -Ou -f ref/Reference.fasta ERR8774458.sorted.bam |bcftools call -Ov -mv > ERR8774458.vcf
+less ERR8774458.vcf
+history
+sudo apt-get install tabix
+bgzip VCF/Result.vcf
+nano Bruno_Project3.sh
+#!/usr/bin/bash
+nano Bruno_Project3.sh
+bash Bruno_Project3.sh
+nano requirement.txt
+nano setup.sh
+chmod +x setup.sh
+./setup.sh
